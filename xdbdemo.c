a95c3b11eb83b5ace7f6921ac5449d1a5c92e446
wuxb45 <wuxb45@gmail.com>
2021-02-26 01:55:35 -0600

    more comments in xdbdemo

--- xdbdemo.c
+++ xdbdemo.c
@@ -14,7 +14,13 @@ main(int argc, char ** argv)
 {
   (void)argc;
   (void)argv;
-  struct xdb * const xdb = remixdb_open("/tmp/xdbdemo", 256, 256); // blockcache=256MB, MemTable=256MB
+  // Use a small config for demo
+  // In a moderate setup the recommended numbers are 4096 for both
+  struct xdb * const xdb = remixdb_open("./xdbdemo", 256, 256); // blockcache=256MB, MemTable=256MB
+
+  // A ref is required to perform the following DB operations.
+  // A thread should maintain a ref and keep using it.
+  // Different threads should use different refs.
   struct xdb_ref * const ref = remixdb_ref(xdb);
 
   bool r;
@@ -43,11 +49,18 @@ main(int argc, char ** argv)
   remixdb_set(ref, "11", 2, "1_value", 7);
   remixdb_set(ref, "22", 2, "2_value", 7);
 
+  // Make all the data persistent in the log.
+  // Performing sync is expensive.
+  remixdb_sync(ref);
+
+  // range operations
   struct xdb_iter * const iter = remixdb_iter_create(ref);
 
-  remixdb_iter_seek(iter, NULL, 0); // seek to the head
-  printf("remixdb_iter_seek \"\"\n");
-  while (remixdb_iter_valid(iter)) {
+  printf("remixdb_iter_seek \"\" (zero-length string)\n");
+  remixdb_iter_seek(iter, NULL, 0); // seek to the first key
+  // You can actually insert an zero-size key to the store. (0 <= klen, klen+vlen <= 65500)
+
+  while (remixdb_iter_valid(iter)) { // check whether the iter points to a valid KV pair
     r = remixdb_iter_peek(iter, kbuf_out, &klen_out, vbuf_out, &vlen_out);
     if (r) {
       printf("remixdb_iter_peek klen=%u key=%.*s vlen=%u value=%.*s\n",
@@ -58,13 +71,17 @@ main(int argc, char ** argv)
     remixdb_iter_skip(iter, 1);
   }
 
-  // call iter_park if you will go idle but want to use the iter later
-  // don't need to call iter_park if you're actively using iter
+  // This is OPTIONAL!
+  // an iter can hold some (reader) locks.
+  // Other (writer) threads can be blocked by active iters.
+  // call iter_park to release those resources when you need to go idle
+  // don't need to call iter_park if you're actively using the iter
   remixdb_iter_park(iter);
   usleep(10);
 
+  // after calling iter_park, you must perform a seek() to proceed with other operations.
+  printf("remixdb_iter_seek \"0\" (key_length=1)\n");
   remixdb_iter_seek(iter, "0", 1);
-  printf("remixdb_iter_seek \"0\"\n");
   // this time we don't want to copy the value
   r = remixdb_iter_peek(iter, kbuf_out, &klen_out, NULL, NULL);
   if (r){
@@ -74,10 +91,11 @@ main(int argc, char ** argv)
   }
 
   remixdb_iter_destroy(iter);
-  // there must be no active iter when calling unref()
+  // there must be no active iters when we call unref()
   remixdb_unref(ref);
 
-  // unsafe operations: should have released all references
-  remixdb_close(xdb); // destroy also calls clean interally
+  // close is not thread-safe
+  // other threads must have released their references when you call close()
+  remixdb_close(xdb);
   return 0;
 }
